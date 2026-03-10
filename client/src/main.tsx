d6a4386b2cb7b0a6bea1023dcde60132409dfad2
Bill Wright <jordanad46@gmail.com>
2026-03-09 20:31:52 -0400

    Integrate ThemeProvider and React.StrictMode
    Refactor main.tsx to use ThemeProvider and React.StrictMode.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ThemeProvider } from './context/ThemeContext.tsx';  // adjust path
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
 
 createRoot(document.getElementById("root")!).render(<App />);
